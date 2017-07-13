f33ab1ab4699eb455a8a300d559862178a96ec74
David CASTRO-CHEW <dcastro-@e1z2r6p3.42.us.org>
2017-07-12 23:03:24 -0700

    file check working

--- srcs/check_valid.c
+++ srcs/check_valid.c
@@ -6,27 +6,28 @@
 /*   By: dcastro- <dcastro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 14:36:09 by dcastro-          #+#    #+#             */
-/*   Updated: 2017/07/12 22:28:33 by dcastro-         ###   ########.fr       */
+/*   Updated: 2017/07/12 23:02:41 by dcastro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-static int line_check(char *s)
-{
-	int i;
+// static int line_check(char *s)
+// {
+// 	int i;
 
-	i = 0;
-	while (s[i])
-	{
-		if ((ft_isalpha(s[i])))
-			return (0);
-		if (s[i] == '-' &&  !ft_isdigit(s[i + 1]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
+// 	i = 0;
+// 	printf("here: %s\n", s);
+// 	while (s[i])
+// 	{
+// 		if ((ft_isalpha(s[i])))
+// 			return (0);
+// 		if (s[i] == '-' &&  ((s[ + 1] <= '0' || s[i + 1] >= '9')))
+// 			return (0);
+// 		i++;
+// 	}
+// 	return (1);
+// }
 
 static int get_width(char **str)
 {
@@ -38,18 +39,28 @@ static int get_width(char **str)
 	return (i);
 }
 
-static int check_values(int fd, char *line)
+static int check_values(int fd)
 {
 	int w;
 	int w2;
+	int i;
 	char **tmp;
+	char *line;
 
 	w = 0;
 	w2 = 0;
+	line = NULL;
+	i = 0;
 	if (get_next_line(fd, &line))
 	{
-		if (!(line_check(line)))
-			return (0);
+		while (line[i])
+		{	
+			if ((ft_isalpha(line[i])))
+				return (0);
+			if (line[i] == '-' &&  ((line[ + 1] <= '0' || line[i + 1] >= '9')))
+				return (0);
+			i++;
+		}
 		if (!(tmp = ft_strsplit(line, ' ')))
 			return (0);
 		if ((!(w = get_width(tmp))))
@@ -61,11 +72,18 @@ static int check_values(int fd, char *line)
 		return (0);
 	while (get_next_line(fd, &line))
 	{
-		if (!(line_check(line)))
-			return (0);
+		i = 0;
+		while (line[i])
+		{	
+			if ((ft_isalpha(line[i])))
+				return (0);
+			if (line[i] == '-' &&  ((line[ + 1] <= '0' || line[i + 1] >= '9')))
+				return (0);
+			i++;
+		}
 		if (!(tmp = ft_strsplit(line, ' ')))
 			return (0);
-		if ((w2 = get_width(tmp) != w))
+		if ((w2 = get_width(tmp)) != w)
 			return (0);
 		free(tmp);
 		ft_strdel(&line);
@@ -75,14 +93,12 @@ static int check_values(int fd, char *line)
 int check_file(char *av)
 {
 	int fd;
-	char *line;
 
-	line = NULL;
 	if (!av)
 		return (0);
 	if ((fd = open(av, O_RDONLY)) < 0)
 		return (0);
-	if (!(check_values(fd, line)))
+	if (!(check_values(fd)))
 		return (0);
 	return (1);
 }
